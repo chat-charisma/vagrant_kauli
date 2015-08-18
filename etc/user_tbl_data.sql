20f7ef57aff4449a79b33a36f9e5b6d9a5d15242
furukij <furukij@kau.li>
2015-08-18 14:56:40 +0900

    DEV

--- etc/user_tbl_data.sql
+++ etc/user_tbl_data.sql
@@ -25,3 +25,30 @@ INSERT INTO user_tbl(
   1,
   'http://kau.li/'
 );
+INSERT INTO user_tbl(
+  u_mail,
+  u_password,
+  u_name,
+  u_kana,
+  u_country,
+  u_sex,
+  u_birth,
+  u_zip,
+  u_address,
+  u_tel,
+  u_admin,
+  u_url
+)VALUES(
+  'furukij+sponsor@kau.li',
+  'eaa12a81887174c45a365508898a5475dd847452',
+  'かりすま　スポンサー',
+  'かりすま　すぽんさー',
+  'JP',
+  0,
+  '1978-09-23',
+  '2040024',
+  '清瀬市梅園３−１９−５−２０２',
+  '09028103716',
+  1,
+  'http://kau.li/'
+);
