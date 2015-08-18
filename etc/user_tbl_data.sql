fda4f10cc819ada8d47290e7900bcf5079f4383e
furukij <furukij@kau.li>
2015-08-18 14:44:15 +0900

    dev

---
+++ etc/user_tbl_data.sql
@@ -0,0 +1,27 @@
+INSERT INTO user_tbl(
+  u_mail,
+  u_password,
+  u_name,
+  u_kana,
+  u_country,
+  u_sex,
+  u_birth,
+  u_zip,
+  u_address,
+  u_tel,
+  u_admin,
+  u_url
+)VALUES(
+  'furukij@kau.li',
+  'eaa12a81887174c45a365508898a5475dd847452',
+  '古木　淳史',
+  'ふるき　じゅんじ',
+  'JP',
+  0,
+  '1978-09-23',
+  '2040024',
+  '清瀬市梅園３−１９−５−２０２',
+  '09028103716',
+  1,
+  'http://kau.li/'
+);
